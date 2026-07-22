5a695e244348f678cb1e86bf716f302894bf474e
jihanejador <jihanejador@gmail.com>
2026-07-22 16:28:20 +0100

    etudiant faire le reservation

--- database/seeders/ReservationSeeder.php
+++ database/seeders/ReservationSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Event;
+use App\Models\Reservation;
 
 class ReservationSeeder extends Seeder
 {
@@ -12,6 +15,14 @@ class ReservationSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $etudiant = User::where('role', 'etudiant')->first();
+        $event = Event::first();
+
+        if ($etudiant && $event) {
+            Reservation::create([
+                'user_id' => $etudiant->id,
+                'event_id' => $event->id,
+            ]);
+        }
     }
 }
