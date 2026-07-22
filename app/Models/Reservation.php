f0d2fc455f0b68486d4be5c844988a520984ed2f
jihanejador <jihanejador@gmail.com>
2026-07-22 11:57:21 +0100

    faire le relation entre le reservation et le user

--- app/Models/Reservation.php
+++ app/Models/Reservation.php
@@ -7,5 +7,9 @@ use Illuminate\Support\str;
 
 class Reservation extends Model
 {
-    //
+    protected $fillable =['numero_reservation', 'user_id', 'event_id'];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
 }
