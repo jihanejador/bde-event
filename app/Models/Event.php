6d294d0762e47e7e6ca87cdaf04a19dc1a60f75c
jihanejador <jihanejador@gmail.com>
2026-07-22 12:08:21 +0100

    faire la relation entre event et user

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -7,5 +7,13 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Event extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'titre', 'description', 'date', 'heure',
+        'lieu', 'prix', 'jauge_max', 'user_id'
+    ];
+    public function admin(){
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
