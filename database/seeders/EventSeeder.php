58d388f73cde446fef69dfeaed3a94b04f5fb1e4
jihanejador <jihanejador@gmail.com>
2026-07-22 16:25:48 +0100

    est ce que le admin faire login ils faire creation de event

--- database/seeders/EventSeeder.php
+++ database/seeders/EventSeeder.php
@@ -13,6 +13,18 @@ class EventSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $admin = User::where('role', 'admin')->first();
+        if($admin){
+            Event::create([
+                'titre' =>'Soiree d\'Integration BDE 2026',
+                'description' => 'Soirée d\'accueil des nouveaux étudiants avec animations et buffet.',
+                'date' => '2026-09-15',
+                'heure' => '18:00',
+                'lieu' => 'Grand Amphithéâtre',
+                'prix' => 0.00,
+                'jauge_max' => 50,
+                'user_id' => $admin->id,
+            ]);
+        }
     }
 }
