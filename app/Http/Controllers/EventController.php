07dc59aee638a74ed07a30cdf64b1422f5858191
jihanejador <jihanejador@gmail.com>
2026-07-23 16:36:42 +0100

    creation un condition pour securiser lacces admin

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -7,5 +7,9 @@ use App\Models\Event;
 
 class EventController extends Controller
 {
-    //
+    public function adminIndex(){
+        if(auth()->user()->role !== 'admin'){
+            abort(403, 'Acces refuse : Espace reserve a l\'admin.');
+        }
+    }
 }
