f90ca71721ca89aec624b247fb856fb2b9e446c3
jihanejador <jihanejador@gmail.com>
2026-07-23 15:34:54 +0100

    faire le route de redirect home a login

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controller\AuthController;
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::get('/', function(){
+    return redirect()->route('login');
 });
