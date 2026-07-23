54e056505717218de079b90f6f14a31a8d094a37
jihanejador <jihanejador@gmail.com>
2026-07-23 15:40:47 +0100

    creation le route de login avec middleware

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,9 @@ use App\Http\Controller\AuthController;
 Route::get('/', function(){
     return redirect()->route('login');
 });
+
+Route::middleware('guest')->group(function (){
+    Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+    Route::post('/login', [AuthController::class, 'login']);
+
+});
