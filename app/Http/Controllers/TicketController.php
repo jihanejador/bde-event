9c3956d6591eb5ede189d2c70f5deda16fbc0a13
jihanejador <jihanejador@gmail.com>
2026-07-24 15:43:49 +0100

    sffichage des billets de l'etudiant connecte

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -7,5 +7,12 @@ use App\Models\Reservation;
 
 class TicketController extends Controller
 {
-    //
+    public function index(){
+        $reservations = Reservation::with('event')
+            ->where('user_id', auth()->id())
+            ->latest()
+            ->get();
+
+        return view('student.tickets.index', compact('reservations'));
+    }
 }
