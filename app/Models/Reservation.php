e8f0cc8419567740218976e01d9083586f8e8bab
jihanejador <jihanejador@gmail.com>
2026-07-24 12:26:15 +0100

    creation de reservation

--- app/Models/Reservation.php
+++ app/Models/Reservation.php
@@ -2,27 +2,28 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\str;
 
 class Reservation extends Model
 {
-    protected $fillable =['numero_reservation', 'user_id', 'event_id'];
+    use HasFactory;
 
-    public function user(){
+    protected $fillable = [
+        'user_id',
+        'event_id',
+        'numero_reservation',
+    ];
+
+    
+    public function user()
+    {
         return $this->belongsTo(User::class);
     }
 
-    public function event(){
-        return $this->belongsTo(Event::class);
-    }
-    protected static function boot(){
-        parent::boot();
 
-        static::creating(function ($reservation){
-            if (empty($reservation->numero_reservation)){
-                $reservation->numero_reservation = 'BDE-2026'.strtoupper(Str::random(5));
-            }
-        });
+    public function event()
+    {
+        return $this->belongsTo(Event::class);
     }
 }
