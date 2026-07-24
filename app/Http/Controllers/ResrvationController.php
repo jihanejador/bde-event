9f2146c8732f6709153294d5018d14f8cf1c1b4a
jihanejador <jihanejador@gmail.com>
2026-07-24 12:29:22 +0100

    importer les cless qui utilise sur ReservationController

--- app/Http/Controllers/ResrvationController.php
+++ app/Http/Controllers/ResrvationController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Event;
+use App\Models\Reservation;
+use Illuminate\Support\Str;
 
 class ResrvationController extends Controller
 {
